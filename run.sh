732a92ba9e94ebdad0d7a9f8b8134b43e4230609
Logan Ford <retrogamer500@gmail.com>
2020-02-20 11:26:02 -0500

    Changes to httpd config

--- run.sh
+++ run.sh
@@ -1,4 +1,13 @@
-currentdir=$(echo $(pwd) | sed 's/^\/c\//C:\//g')
-export V_ROOT=$currentdir/volumes/
-export LFN_ROOT=$currentdir/loganfordnet
-docker-compose up --build
+DOCKER_COMPOSE_ARGS=""
+
+while getopts ":b" opt; do
+    case ${opt} in
+        b ) DOCKER_COMPOSE_ARGS="--build" && echo "Rebuilding docker containers" ;;
+        \? ) echo "Usage: cmd [-b]" ;;
+    esac
+done
+
+CURRENT_DIR=$(echo $(pwd) | sed 's/^\/c\//C:\//g')
+export V_ROOT=$CURRENT_DIR/volumes/
+export LFN_ROOT=$CURRENT_DIR/loganfordnet
+docker-compose up $DOCKER_COMPOSE_ARGS
